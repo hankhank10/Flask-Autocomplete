bb802cdab559cbf2427f4a9c765f27dbb4015451
hankhank10 <m.a.hankinson@gmail.com>
2020-12-26 10:52:10 +0000

    Updated version removing use of wtf and showing how multiple lists could be called

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, Response, render_template, request
 import json
-from wtforms import TextField, Form
 
 app = Flask(__name__)
 
@@ -14,20 +13,26 @@ cities = ["Bratislava",
           "Zvolen",
           "Poprad"]
 
+countries = ["UK",
+             "France",
+             "Poland"]
 
-class SearchForm(Form):
-    autocomp = TextField('Insert City', id='city_autocomplete')
 
+@app.route('/_autocomplete/<what_to_get>', methods=['GET'])
+def autocomplete(what_to_get):
+    
+    if what_to_get == "cities":
+        output = cities
 
-@app.route('/_autocomplete', methods=['GET'])
-def autocomplete():
-    return Response(json.dumps(cities), mimetype='application/json')
+    if what_to_get == "countries":
+        output = countries
+    
+    return Response(json.dumps(output), mimetype='application/json')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = SearchForm(request.form)
-    return render_template("search.html", form=form)
+    return render_template("search.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
